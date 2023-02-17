56289f102ea6aa2c62bbc80472a58eaff46e1f25
Carlos Reyes <careyespe26@gmail.com>
2023-02-17 01:32:44 -0500

    esquemas para envio de datos

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 from fastapi import FastAPI
 from fastapi import Body
+from pydantic import BaseModel
+from typing import Optional
 
 from fastapi.responses import HTMLResponse
 
@@ -8,6 +10,14 @@ app = FastAPI()
 # y los parametros de abajo son los editables
 app.title = "Mi aplicacion con FastAPI"
 app.version = "0.0.1"
+
+class Movie(BaseModel):
+    id: Optional[int] = None
+    title: str
+    overview: str
+    year: int
+    rating: float
+    category: str
 
 movies = [
     {
@@ -63,27 +73,20 @@ def get_movies_by_category(category: str, year: int):
 
 
 @app.post('/movies', tags=['movies'])
-def create_movie(id: int = Body(), title: str = Body(), overview: str = Body(), year: int = Body(), rating: float = Body(), category: str = Body()):
-    movies.append({
-        'id': id,
-        'title': title,
-        'overview': overview,
-        'year': year,
-        'rating': rating,
-        'category': category
-    })
+def create_movie(movie: Movie):
+    movies.append(movie)
     return movies
 
 
 @app.put('/movie/{id}', tags=['movies'])
-def update_movie(id: int, title: str = Body(), overview: str = Body(), year: int = Body(), rating: float = Body(), category: str = Body()):
+def update_movie(id: int, movie: Movie):
             for item in movies:
                 if item['id'] == id:
-                    item['title'] = title
-                    item['overview'] = overview
-                    item['year'] = year
-                    item['rating'] = rating
-                    item['category'] = category
+                    item['title'] = movie.title
+                    item['overview'] = movie.overview
+                    item['year'] = movie.year
+                    item['rating'] = movie.rating
+                    item['category'] = movie.category
 
             return movies
 
